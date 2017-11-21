ce3d76670dcc19c43d0cea753b96821ce9eefdfd
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-21 03:38:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
-def prime?(num)
-  num_array = (2..(num-1)).to_a
-  num_array.find do |div|
-    if num%div == 0
-      return false
-    elsif num <= 1
-      return false
-    else
-      return true
+  def prime?(num)
+    num_array = (2..(num-1)).to_a
+    num_array.find do |div|
+      if num%div == 0
+        return false
+      elsif num < 2
+        return false
+      else
+        return true
+      end
     end
   end
-end
