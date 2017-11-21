d8ed509f9507a9a9c6db787779d028d662a877cf
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-21 03:53:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,7 @@ def prime?(num)
     num_array = (2..(num-1)).to_a
     num_array.find do |div|
       if num%div == 0
-        return false
-      else
-        return true
+      
       end
     end
   end
